bbe652ac3148f9a1d5d03e4298dc359fe8bc63b8
mmarkk <mmarkk@xtra.co.nz>
2023-12-28 10:17:19 +1300

    Add selected property to each item in the list and update totalWeight calculation

--- src/routes/three/+page.svelte
+++ src/routes/three/+page.svelte
@@ -6,28 +6,52 @@
 
 	let list = data.items;
 
+	// add selected: false to each item in the list
+	list = list.map((item) => {
+		return {
+			...item,
+			selected: false
+		};
+	});
+
 	$: totalWeight = 0;
 
-	function addWeight(weight: number) {
-		totalWeight += weight;
+	function addWeight(item) {
+		if (item.selected) {
+			item.selected = false;
+			totalWeight -= item.weight;
+		} else {
+			item.selected = true;
+			totalWeight += item.weight;
+		}
 	}
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5 m-8">
-		<h1 class="h1">Jingle Bell Balancer</h1>
-		{#if totalWeight < 100}
-			<h1 class="h1">Total Weight in Sleigh: {totalWeight} kgs</h1>
-		{:else}
-			<h1 class="h1 text-red-500 font-bold">Sleigh Overloaded! Total Weight: {totalWeight} kgs</h1>
-		{/if}
+	<div class="card m-4 bg-tertiary-200">
+		<div class="card-body">
+			<div class="space-y-5 m-8">
+				<h1 class="h1">Jingle Bell Balancer</h1>
+				{#if totalWeight < 100}
+					<h1 class="h1">Total Weight in Sleigh: {totalWeight.toFixed(1)} kgs</h1>
+				{:else}
+					<h1 class="h1 text-red-500 font-bold">
+						Sleigh Overloaded! Total Weight: {totalWeight.toFixed(1)} kgs
+					</h1>
+				{/if}
+			</div>
+		</div>
 	</div>
 </div>
 
-<div class="grid grid-cols-3">
-	{#each list as listItem, i}
-		<SlideToggle name="slider-label" class="m-3" on:click={() => addWeight(listItem.weight)}
-			>{listItem.name}</SlideToggle
-		>
-	{/each}
+<div class="card m-4 bg-tertiary-200">
+	<div class="card-body">
+		<div class="grid grid-cols-3">
+			{#each list as listItem, i}
+				<SlideToggle name="slider-label" class="m-3 " on:click={() => addWeight(listItem)}
+					>{listItem.name}</SlideToggle
+				>
+			{/each}
+		</div>
+	</div>
 </div>
